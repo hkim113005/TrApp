210821963da744306ddc8e712ea40c8cd2e578ff
hkim113005 <70756913+hkim113005@users.noreply.github.com>
2023-03-02 09:43:53 +0400

    Swap Optimize Algorithm v1 Completed

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import math
 import random
+import copy
+from tqdm import tqdm
+import time
 
-SIZE = 100
+SIZE = 12
 GROUP_SIZE = 3
 NUM_GROUP = math.ceil(SIZE / GROUP_SIZE)
 
@@ -20,6 +23,9 @@ class Group:
 
     def add(self, member):
         self.members.append(member)
+
+    def remove(self, member):
+        self.members.remove(member)
 
     def add_best(self, students, preferences):
         student = self.get_best(students, preferences)
@@ -49,40 +55,66 @@ class Group:
         
         return highest_student
 
-for i in range(10):
-    students = []
-    preferences = []
+students = []
+preferences = []
 
-    for i in range(SIZE):
-        temp = []
-        while len(temp) < 5:
-            num = random.randint(0, SIZE - 1)
-            if num not in temp and num != i:
-                temp.append(num)
-        students.append(i)
-        preferences.append(temp)
-        
+best_result = 100
+
+"""
+for i in range(SIZE):
+    temp = []
+    while len(temp) < 5:
+        num = random.randint(0, SIZE - 1)
+        if num not in temp and num != i:
+            temp.append(num)
+    preferences.append(temp)
+"""
+
+preferences = [
+    [5, 6, 3, 10, 4],
+    [0, 6, 8, 7, 4],
+    [11, 0, 4, 8, 1],
+    [1, 6, 8, 0, 2],
+    [10, 0, 1, 6, 2],
+    [3, 7, 10, 8, 9],
+    [9, 3, 2, 11, 4],
+    [8, 1, 10, 2, 6],
+    [6, 4, 1, 0, 2],
+    [10, 6, 1, 3, 5],
+    [4, 0, 8, 3, 11],
+    [9, 6, 2, 7, 8]
+]
+    
+for preference in preferences:
+    print(preference)
+
+print()
+
+for i in tqdm(range(100)):
+    for j in range(SIZE):
+        students.append(j)
+
     groups = []
     for i in range(NUM_GROUP):
         groups.append(Group())
 
     random.shuffle(students)
 
-    for i in range(NUM_GROUP):
-        groups[i].add(students.pop())
+    for j in range(NUM_GROUP):
+        groups[j].add(students.pop())
 
     while len(students) != 0:
-        for i in range(NUM_GROUP):
-            if groups[i].get_size() != GROUP_SIZE and len(students) != 0:
-                students.remove(groups[i].add_best(students, preferences))
-
+        for j in range(NUM_GROUP):
+            if groups[j].get_size() != GROUP_SIZE and len(students) != 0:
+                students.remove(groups[j].add_best(students, preferences))
+    
     results = [0, 0, 0, 0]
 
-    for i in range(NUM_GROUP):
+    for j in range(NUM_GROUP):
         count = 0
-        for member1 in groups[i].members:
+        for member1 in groups[j].members:
             satisfied = False
-            for member2 in groups[i].members:
+            for member2 in groups[j].members:
                 if member2 in preferences[member1]:
                     satisfied = True
                     break
@@ -91,9 +123,49 @@ for i in range(10):
 
         results[count] += 1
 
-    print(results)
+    for j in range(15):
+        for group1 in groups:
+            for member1 in group1.members:
+                for group2 in groups:
+                    if group1 == group2:
+                        continue
+                    for member2 in group2.members:
+                        current_value = group1.get_value(preferences) + group2.get_value(preferences)
+                        group1.remove(member1)
+                        group2.remove(member2)
+                        group1.add(member2)
+                        group2.add(member1)
+                        possible_value = group1.get_value(preferences) + group2.get_value(preferences)
+                        if current_value >= possible_value:
+                            group1.remove(member2)
+                            group2.remove(member1)
+                            group1.add(member1)
+                            group2.add(member2)
+                        else:
+                            temp = member1
+                            member1 = member2
+                            member2 = temp
 
-    for i in range(NUM_GROUP):
-        for j in range(NUM_GROUP):
-            if i == j: continue
+    results = [0, 0, 0, 0]
+    unhappy = 0
+
+    for j in range(NUM_GROUP):
+        count = 0
+        for member1 in groups[j].members:
+            satisfied = False
+            for member2 in groups[j].members:
+                if member1 == member2:
+                    continue
+                if member2 in preferences[member1]:
+                    satisfied = True
+                    break
+            if satisfied:
+                count += 1
+
+        unhappy += groups[j].get_size() - count
+
+        results[count] += 1
+
+    best_result = min(best_result, unhappy)
             
+print(best_result)
