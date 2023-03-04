8f8b544f71e7ed6aa763f0a2313accfa73036340
hkim113005 <70756913+hkim113005@users.noreply.github.com>
2023-03-04 22:23:11 +0400

    Completed verify_email.py

--- verify_email.py
+++ verify_email.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
-valid_emails = pd.read_csv("data/acs_emails.csv", usecols=["email"], dtype=str)
-emails = pd.read_csv("data/T")
-print(emails)
+valid_emails = []
+for index, row in pd.read_csv("data/acs_emails.csv", usecols=["email"], dtype=str).iterrows():
+    valid_emails.append(row["email"])
+
+students = pd.read_csv("data/students.csv", usecols=["name", "email"], dtype=str)
+
+for index, row in students.iterrows():
+    if row["email"].lower() not in valid_emails:
+        print(row["email"].lower())
+        print(row["name"])
