81853aceb3ec7b2fae88fd23f07958da44650f8a
hkim113005 <70756913+hkim113005@users.noreply.github.com>
2023-03-04 22:08:43 +0400

    Organization.

--- verify_email.py
+++ verify_email.py
@@ -1,34 +1,3 @@
-import re
-import dns.resolver
-import smtplib
+import verify_email as vf
 
-def verify_email(email):
-    # Verify email format
-    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-        return False
-    
-    # Extract domain from email
-    domain = email.split('@')[1]
-    
-    try:
-        # Check domain MX records
-        records = dns.resolver.resolve(domain, 'MX')
-        mx_record = records[0].exchange.to_text()
-
-        # Connect to SMTP server
-        smtp_server = smtplib.SMTP()
-        smtp_server.connect(mx_record)
-        smtp_server.helo(smtp_server.local_hostname)
-        smtp_server.mail('test@test.com')
-        response = smtp_server.rcpt(email)
-        smtp_server.quit()
-
-        # Email is valid if SMTP server accepts it
-        if response[0] == 250:
-            return True
-        else:
-            return False
-    except:
-        return False
-
-print(verify_email("hyungjae1130@gmail.com"))
+print(vf.verify_email("hyungjae1130@gmail.com"))
