3c4abc079f461dc2fe54e9df61d6cd6346e0e0a7
jmathew777 <98299987+jmathew777@users.noreply.github.com>
2023-03-11 22:56:34 +0400

    numerical group info auto calculates

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ db = Database()
 # REMOVE LATER: Adds trip to database
 db.addTrip(Trip(None, "WWW 2023: Grade 6 (Greece)", "MS", 4, 2, "blah blah blah", [1, 2, 3, 4]))
 db.addTrip(Trip(None, "Viper Venture 2023: Thailand", "HS", 5, 3, "idk lol", [260, 261, 262]))
-db.addTrip(Trip(None, "JV Boys Volleyball", "MESAC", 7, 2, "eeeeeee", [148, 100, 123, 90, 7]))
-db.addTrip(Trip(None, "Varsity Boys Soccer", "MESAC", 9, 3, "aaaaa", [21, 150, 230, 190, 72, 110]))
+db.addTrip(Trip("TEST11", "JV Boys Volleyball", "MESAC", 7, 2, "eeeeeee", [148, 100, 123, 90, 7,21, 150, 230, 190, 72, 110]))
+db.addTrip(Trip(None, "Varsity Boys Soccer", "MESAC", 9, 3, "aaaaa", [21, 150, 230]))
 db.addTrip(Trip(None, "HS Track & Field", "MESAC", 3, 3, "yyyyyy", [273, 220]))
 db.addTrip(Trip(None, "HS Tennis", "MESAC", 5, 2, "xxxxxxx", [288, 270, 242, 276]))
 db.addTrip(Trip(None, "HS Wrestling", "MESAC", 6, 2, "wwwwww", [204]))
@@ -70,18 +70,32 @@ def trip_code_form():
         fourth = request.form["fourth"]
         fifth = request.form["fifth"]
         sixth = request.form["sixth"]
-
         return redirect("/" + first + second + third + fourth + fifth + sixth)
     
 @app.route("/<trip_id>", methods=["GET", "POST"])
 def student_preference_form(trip_id):
     if request.method == "GET":
         if db.getTripById(trip_id) != None:
-            return render_template("student_preference_form.html", sel_trip = db.getTripById(trip_id), sel_students = db.getStudentsInTrip(trip_id), num_prefs = 5 if len(db.getStudentsInTrip(trip_id)) - 1 > 5 else len(db.getStudentsInTrip(trip_id)) - 1)
+            return render_template("student_preference_form.html", trip_id=trip_id, sel_trip = db.getTripById(trip_id), sel_students = db.getStudentsInTrip(trip_id), num_prefs = 5 if len(db.getStudentsInTrip(trip_id)) - 1 > 5 else len(db.getStudentsInTrip(trip_id)) - 1)
         else:
             return render_template("error.html")
     else:
-        return render_template("success.html", trip_id=trip_id)
+        self_id = pref_1 = pref_2 = pref_3 = pref_4 = pref_5 = 0
+        if 'pref_0' in request.form:
+            self_id = request.form['pref_0']
+        if 'pref_1' in request.form:
+            pref_1 = request.form['pref_1']
+        if 'pref_2' in request.form:
+            pref_2 = request.form['pref_2']
+        if 'pref_3' in request.form:
+            pref_3 = request.form['pref_3']
+        if 'pref_4' in request.form:
+            pref_4 = request.form['pref_4']
+        if 'pref_5' in request.form:
+            pref_5 = request.form['pref_5']
+        print(self_id, pref_1, pref_2, pref_3, pref_4, pref_5)
+        
+        return render_template("success.html", sel_trip = db.getTripById(trip_id))
 
 @app.route("/teacher-login", methods=["GET", "POST"])
 def teacher_login_form():
