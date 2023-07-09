1f7cec9bb5618ce33bdbb1f1b2bd66b60951dd14
jmathew777 <98299987+jmathew777@users.noreply.github.com>
2023-07-09 19:55:34 +0400

    Preference Email HTML Fix
    Fixed some placeholder text on student preference email html, updated email formatting.

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-SESSION_TYPE = "filesystem"
-
 SQLALCHEMY_DATABASE_URI = "sqlite:///../data/TrApp.db"
 SECRET_KEY = "ACS-TSU@2023.VerYvERySeCreT"
 
@@ -7,6 +5,6 @@ MAIL_SERVER = "smtp.gmail.com"
 MAIL_DEFAULT_SENDER = "TSU TrApp"
 MAIL_USERNAME = "acstsutesting@gmail.com" # Change to tsu@acs.sch.ae after testing finished
 MAIL_PASSWORD = "xbuqjxaljpqpenoz" # App Password - https://myaccount.google.com/u/apppasswords
-MAIL_PORT = 465
-MAIL_USE_SSL = True
-MAIL_USE_TLS = False
+MAIL_PORT = 587
+MAIL_USE_SSL = False # Port 465
+MAIL_USE_TLS = True # Port 587
